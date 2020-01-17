17a433fe15ff1230c455615ca79c683961037d0a
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-01-17 21:59:03 +0100

    Create  class component with state

--- App.js
+++ App.js
@@ -8,13 +8,36 @@ function Greetings({ name }) {
   )
 }
 
+class Blink extends Component {
+  componentDidMount(){
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+  state = { isShowingText: true };
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
 export default function App() {
   let pic = {
     uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
   }
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Greetings name="Mateusz" />
+      <Greetings name="Daria" />
+      <Greetings name='Rexxar' />
+      <Greetings name='Jaina' />
+      <Greetings name='Valeera' />
+      <Blink text='I love to blink' />
       <Image source={pic} style={{width: 193, height: 110}} />
   </View>
   );
