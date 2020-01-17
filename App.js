785d7d863c066f652c3a9e23a6df69e36a685f5b
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-01-17 22:19:18 +0100

    style boxes with flexbox

--- App.js
+++ App.js
@@ -1,61 +1,29 @@
 import React, { Component, useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-
-function Greetings({ name }) {
-  return(
-    <Text>Hello, { name }!</Text>
-  )
-}
-
-class Blink extends Component {
-  componentDidMount(){
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
-  }
-  state = { isShowingText: true };
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-    textDecorationStyle: 'dotted',
-    textDecorationLine: "underline",
-    textDecorationColor: "black"
+  box: {
+    height: 100,
+    flex: 1,
   },
-  red: {
-    color: 'red',
-    opacity: 0.4
-  }
+  firstBox: {
+    backgroundColor: "red",
+    flex:2
+  },
+  secondBox: {
+    backgroundColor: "blue",
+  },
+  thirdBox: {
+    backgroundColor: "yellow",
+  },
 })
 
 export default function App() {
-  let pic = {
-    uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-  }
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Greetings name='Jaina' />
-      <Greetings name='Valeera' />
-      <Image source={pic} style={{width: 193, height: 110}} />
-      <Text style={styles.red}>just red</Text>
-      <Text style={styles.bigBlue}>just bigBlue</Text>
-      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>      
-      <Blink text='I love to blink' />
-  </View>
+    <View style={{ flex: 1 }}>
+      <View style={[styles.box, styles.firstBox]}/>
+      <View style={[styles.box, styles.secondBox]}/>
+      <View style={[styles.box, styles.thirdBox]}/>
+    </View>
   );
 }
