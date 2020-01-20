8575b928de02b1546c8b12e6606a25db00f4a427
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-01-20 21:36:38 +0100

    Add touch handlers

--- App.js
+++ App.js
@@ -1,58 +1,119 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { render } from 'react-dom';
+import React, { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback
+} from "react-native";
+import { render } from "react-dom";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "center"
   },
   buttonContainer: {
     margin: 20
   },
   box: {
     height: 100,
-    flex: 1,
+    flex: 1
   },
   firstBox: {
     backgroundColor: "red",
-    flex:2
+    flex: 2
   },
   secondBox: {
-    backgroundColor: "blue",
+    backgroundColor: "blue"
   },
   thirdBox: {
-    backgroundColor: "yellow",
+    backgroundColor: "yellow"
   },
-})
+  button: {
+    marginBottom: 20,
+    width: 260,
+    alignItems: "center",
+    backgroundColor: "#2196F3",
+  },
+  buttonText: {
+    textAlign: "center",
+    padding: 20,
+    color: "white"
+  }
+});
 
 function TextDisplay() {
   const [text, setText] = useState("");
   return (
     <View>
       <TextInput
-        style={{height: 120, background: "lightblue"}}
+        style={{ height: 120, background: "lightblue" }}
         placeholder="Type here to display text below!"
-        onChangeText={(text) => setText(text)}
+        onChangeText={text => setText(text)}
         value={text}
       />
       <Text>{text}</Text>
     </View>
-  )
+  );
 }
 
 function ButtonBasic() {
-  const _onPressButton = () =>  {
-    alert("You tapped button!")
-  }
-  
+  const _onPressButton = () => {
+    alert("You tapped button!");
+  };
+
   return (
     <View style={styles.buttonContainer}>
-      <Button
+      <Button onPress={_onPressButton} title="Press Me" color="#841584" />
+    </View>
+  );
+}
+
+function Touchables() {
+  const _onPressButton = () => {
+    alert("You pressed the button")
+  }
+
+  const _onLongPressButton = () => {
+    alert("You long pressed the button!")
+  } 
+
+  return (
+    <View>
+      <TouchableHighlight
         onPress={_onPressButton}
-        title="Press Me"
-        color="#841584"
-      />
+        onLongPress={_onLongPressButton}
+        underlayColor="white"  
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>
+            TouchableHighlight
+          </Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableOpacity
+        onPress={_onPressButton}
+      >
+       <View style={styles.button}>
+          <Text style={styles.buttonText}>
+            TouchableOpacity
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableWithoutFeedback
+        onPress={_onPressButton}
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>
+            TouchableWithoutFeedback
+          </Text>
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   )
 }
@@ -60,12 +121,11 @@ function ButtonBasic() {
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <View style={[styles.box, styles.firstBox]}>
-        
-      </View>
-      <ButtonBasic />
-      <View style={[styles.box, styles.secondBox]}/>
-      <View style={[styles.box, styles.thirdBox]}/>
+      <View style={[styles.box, styles.firstBox]}></View>
+        <ButtonBasic />
+        <Touchables />
+      <View style={[styles.box, styles.secondBox]} />
+      <View style={[styles.box, styles.thirdBox]} />
     </View>
   );
 }
